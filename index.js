c38ea484055d0e28c54a1d59d065a3fa10a0035a
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-15 21:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Add your functions here
+let newMapNegative= function(src,function(){
+  let r = [];
+  for (let i=0;i<src.length;i++){
+      r.push(-1*src[i])
+    }
+    return r
+  
+})
+
+
+// fnc = function(src){
+//   let r = [];
+//   for (let i=0;i<src.length;i++){
+//       r.push(-1*src[i])
+//     }
+//     return r
+  
+// }
+
+newMapNegative([1,2,3,4,5],fn)
+
+// console.log(newMap([1,2,4]))
+function newMapOr(src,fn){
+  let r = [];
+  fn = function(){
+    for (let i=0;i<src.length;i++){
+      r.push(src[i])
+    }
+    return r
+  }
+  
+  return fn()
+}
+
+
+function newMapTwo(src,fn){
+  let r = [];
+  fn = function(){
+    for (let i=0;i<src.length;i++){
+      r.push(2*src[i])
+    }
+    return r
+  }
+  
+  return fn()
+}
+
+function newMapTwo(src,fn){
+  let r = [];
+  fn = function(){
+    for (let i=0;i<src.length;i++){
+      r.push(src[i]*src[i])
+    }
+    return r
+  }
+  
+  return fn()
+}
